9c138dd8f911dcbf92b9db2663aa46928424b180
physostegia <email=nasvay2281337.aslan2009@gmail.com>
2024-10-28 01:45:23 +0700

    trying

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
+
 use League\Csv\Reader;
+
 class CsvHandler
 {
     private $input_file;
     private $separator;
-    
 
-    function __construct($input_file) 
+
+    function __construct($input_file)
     {
-        $this -> input_file = $input_file;
-        $this -> separator = $this -> DetectDelimiter();
+        $this->input_file = $input_file;
+        $this->separator = $this->DetectDelimiter();
     }
 
     function DetectDelimiter()
@@ -26,9 +28,7 @@ class CsvHandler
             }
 
             return array_search(max($delimiters), $delimiters);
-        } 
-        else 
-        {
+        } else {
             throw new Exception("Не удалось открыть файл: " . $this->input_file);
         }
     }
@@ -36,49 +36,99 @@ class CsvHandler
     function ReadCsv()
     {
         $result = [];
-        if(($file = fopen($this -> input_file, "r")) !== false)
-        {
-            while(($data = fgetcsv($file, 1000, ";")) !== false)
-            {
-                
-                
-                
-                
+        if (($file = fopen($this->input_file, "r")) !== false) {
+            while (($data = fgetcsv($file, 1000, $this -> separator)) !== false) {
+
+
+
+
                 if (!empty($data[0])) {
                     $result[] = $data;
                 }
-                
             }
             fclose($file);
-            
-            
-        }
-        else 
-        {
+        } else {
             throw new Exception("Не удалось открыть файл: " . $this->input_file);
         }
         return $result;
     }
 
-    function WriteCsv($output_file, $data)
+    function WriteCsv($output_file, $data, $separator = null)
     {
-
+        $separator = $separator ?? $this->separator;
         $file = fopen($output_file, 'w');
-        foreach($data as $line)
-        {
-            fputcsv($file, $line, ';');
+        foreach ($data as $line) {
+            fputcsv($file, $line, $separator);
         }
+        fclose($file);
+    }
+}
+
+class TournamentBracketHandler
+{
+    private $memberList = [];
+
+
+    function __construct($memberList)
+    {
+        unset($memberList[0]);
+        $memberList = array_values($memberList);
+        $this->memberList = $memberList;
     }
 
+    function ShuffleMembers()
+    {
+        shuffle($this->memberList);
+    }
+    private function ImitateBracket($bracket)
+    {
+        for ($i = 0; $i < count($bracket); $i + 2) 
+        {
+            $bracket[$i];
+        }
+    }
+    private function FormPreliminaryBracket($preliminaryFightsCount)
+    {
+        $memberList = [];
+        $membersCount = count($this->memberList);
+        for ($i = 0; $i < $preliminaryFightsCount; $i++) 
+        {
+            array_push($memberList, $this->memberList[$i], $this->memberList[$membersCount - ($i + 1)]);
+        }
+        return $memberList;
+    }
 
-    
+    function FormDefaultTournamentBracket()
+    {
+        $membersCount = count($this->memberList);
+        $mainMembersCount = null;
+        $preliminaryFightsCount = null;
 
-}
 
-class TournamentBracketsHandler
-{
+        for ($i = 2; $i <= $membersCount; $i *= 2) 
+        {
 
+            $mainMembersCount = $i;
+        }
+
+        $preliminaryFightsCount = $membersCount % $mainMembersCount;
+
+        if ($preliminaryFightsCount > 0) 
+        {
+            if ($preliminaryFightsCount & 1) 
+            {
+                $preliminaryFightsCount++;
+            }
+
+            $preliminaryBracket = $this->FormPreliminaryBracket($preliminaryFightsCount);
+        }
+        return $preliminaryBracket;
+    }
 }
 $p = new CsvHandler("сетка.csv");
-$m = $p -> ReadCsv();
-echo $p -> DetectDelimiter();
+$m = $p->ReadCsv();
+
+
+$q = new TournamentBracketHandler($m);
+print_r($q->FormDefaultTournamentBracket());
+ 
